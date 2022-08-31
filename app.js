049fcbd831d84173f96a4883455d845e2e3e3c86
samuel_markus <markussamuel060@gmail.com>
2022-08-31 13:51:32 +0100

    add secret by id to the secret page

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema({
   email: String,   
   password: String,
-  googleId:String
+  googleId:String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -103,6 +104,32 @@ app.get("/secrets", function(req, res){
     res.redirect("/login");
   }
 });
+
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  } else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  console.log(req.user.id); 
+  User.findById(req.user.id, function(err, foundUser){
+   if(err){
+    console.log(err);
+   }else{
+    if(foundUser){
+      foundUser.secret = submittedSecret;
+      foundUser.save(function(){
+        res.redirect("/secrets")
+      });
+    }
+   }
+  })
+})
+
 
 app.get("/logout", function(req, res){
   //req.logout();
